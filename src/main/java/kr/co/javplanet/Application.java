6b1a6549320d03b5ee3174ef8740450f5ae6491b
김기업 <rldjqv@gmail.com>
2019-03-22 17:49:44 +0900

    1.톰캣 설정

--- src/main/java/kr/co/javplanet/Application.java
+++ src/main/java/kr/co/javplanet/Application.java
@@ -1,10 +1,10 @@
 package kr.co.javplanet;
 
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +12,25 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
-public class Application {
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		try {
-//			GoodsListDto goodsList = context.getBean(GoodsService.class).getGoodsList(new GoodsParam());
-//			context.getBean(GoodsPickService.class).setGoodsList(goodsList.list);
-		} catch (BeansException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+    }
+   
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
 	}
 }
+
+//public class Application {
+//public static void main(String[] args) {
+//	ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+//	try {
+//	} catch (BeansException e) {
+//		e.printStackTrace();
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	}
+//}
+//}
